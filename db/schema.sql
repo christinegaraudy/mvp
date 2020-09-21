d5dffc3a623dc3ee085cfbd9699271e055d6344b
Christine Garaudy <christinegaraudy@gmail.com>
2020-09-21 13:25:52 -0500

    adds webpack and webpack cli

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE recipe_finder;
+
+USE recipe_finder;
+
+CREATE TABLE recipes (
+  id INT,
+  
+)
--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 require ('dotenv').config();
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.port || 8080;
 
-app.listen(8080, () => {
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(port, () => {
   console.log('Listening on port 8080')
 })
 
